2c7fee3e055becc9e5a818e143974c560b33b796
vsoch <vsoch@users.noreply.github.com>
2021-10-23 15:28:49 -0600

    fix
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,16 @@ fi
 JOBFILE="https://raw.githubusercontent.com/${INPUT_REPO}/${INPUT_MAIN}/${INPUT_FILENAME}"
 TMP=$(mktemp -d)
 
-wget ${JOBFILE} -O "${TMP}/${INPUT_FILENAME}"
+BASENAME=$(basename ${INPUT_FILENAME})
+wget ${JOBFILE} -O "${TMP}/${BASENAME}"
 if [[ "$?" != "0" ]]; then
     printf "Issue getting previous job file ${JOBFILE}\n"
+    exit 1
+fi
+
+JOBFILE="${TMP}/${BASENAME}"
+if [[ ! -f "${JOBFILE}" ]]; then
+    printf "${JOBFILE} does not exist.\n"
     exit 1
 fi
 
@@ -27,7 +34,7 @@ if [ -z ${SLACK_WEBHOOK+x} ]; then
     exit 1
 fi
 
-COMMAND="python ${ACTION_DIR}/find-updates.py update --key ${INPUT_KEY} --original ${TMP}/${INPUT_FILENAME} --updated ${INPUT_FILENAME}"
+COMMAND="python ${ACTION_DIR}/find-updates.py update --key ${INPUT_KEY} --original ${JOBFILE} --updated ${INPUT_FILENAME}"
 
 echo "${COMMAND}"
 
