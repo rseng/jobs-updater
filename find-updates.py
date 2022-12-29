58fd5171c6f0458e581da188463b15e8644599a1
Chad Dougherty <crd@acm.org>
2022-12-28 19:17:56 -0500

    fix name->post after ced3fa3f81cb703e8c0968b9dd144e920cdb4e49
    Signed-off-by: Chad Dougherty <crd@acm.org>

--- find-updates.py
+++ find-updates.py
@@ -271,7 +271,7 @@ def main():
 
         # If we are instructed to deploy to mastodon and have a client
         if args.deploy_mastodon and mastodon_client:
-            message = "New #RSEng Job! %s: %s" % (choice, name)
+            message = "New #RSEng Job! %s: %s" % (choice, post)
             mastodon_client.toot(status=message)
 
         # Don't continue if testing
