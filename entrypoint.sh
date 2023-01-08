911ae972b3eaa12f84bfa75cb0c3395930849863
vsoch <vsoch@users.noreply.github.com>
2023-01-07 20:16:27 -0700

    allow if/or for second filename
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,27 +11,28 @@ if [[ ! -f "${INPUT_FILENAME}" ]]; then
     exit 1
 fi
 
-# Wget the comparison file
-PARENT_SHA=$(git log --pretty=%P -n 1 ${CURRENT_SHA} | cut -d ' ' -f 1)
-JOBFILE="https://raw.githubusercontent.com/${INPUT_REPO}/${PARENT_SHA}/${INPUT_FILENAME}"
-TMP=$(mktemp -d)
-
-BASENAME=$(basename ${INPUT_FILENAME})
-wget ${JOBFILE} -O "${TMP}/${BASENAME}"
-if [[ "$?" != "0" ]]; then
-    printf "Issue getting previous job file ${JOBFILE}\n"
-    exit 1
-fi
-
-JOBFILE="${TMP}/${BASENAME}"
-if [[ ! -f "${JOBFILE}" ]]; then
-    printf "${JOBFILE} does not exist.\n"
-    exit 1
-fi
-
 # If we are verbatim given a previous filename, use it
 if [ ! -z ${INPUT_PREVIOUS_FILENAME} ]; then 
     JOBFILE="${INPUT_PREVIOUS_FILENAME}"
+else
+
+    # Wget the comparison file
+    PARENT_SHA=$(git log --pretty=%P -n 1 ${CURRENT_SHA} | cut -d ' ' -f 1)
+    JOBFILE="https://raw.githubusercontent.com/${INPUT_REPO}/${PARENT_SHA}/${INPUT_FILENAME}"
+    TMP=$(mktemp -d)
+
+    BASENAME=$(basename ${INPUT_FILENAME})
+    wget ${JOBFILE} -O "${TMP}/${BASENAME}"
+    if [[ "$?" != "0" ]]; then
+        printf "Issue getting previous job file ${JOBFILE}\n"
+        exit 1
+    fi
+
+    JOBFILE="${TMP}/${BASENAME}"
+    if [[ ! -f "${JOBFILE}" ]]; then
+        printf "${JOBFILE} does not exist.\n"
+        exit 1
+    fi
 fi
 
 # Required to have slack webhook in environment
